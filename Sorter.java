af0f8e81386d1fe2f768ad6a3f14cb1bb0f9fd81
Seifo100 <seif.mahmoud1011@gmail.com>
2024-02-07 16:20:53 +0300

    Adding the selection sort method

--- Sorter.java
+++ Sorter.java
@@ -1,3 +1,19 @@
 public class Sorter {
     
+public static void selectionSort(int[] arr) {
+    int n = arr.length;
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Swap the found minimum element with the element at index i
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
 }
